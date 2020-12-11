7cd030cd86340031a3820552a6e252385d5d3bec
Rafael Zuquim <rzuquim@inoa.com.br>
2020-12-11 17:38:02 -0300

    wip

--- update-shell.ps1
+++ update-shell.ps1
@@ -2,9 +2,10 @@
 if (Test-Path $PROFILE) { 
     $confirm = Read-Host -Prompt 'There is already a $PROFILE available. Are you sure you want to overwrite it? (y/N)'
     if ($confirm.StartsWith('y') -or  $confirm.StartsWith('Y')) {
+        $profileDir = Split-Path $PROFILE
         write-host "cp .\windows_env\profile.ps1 $PROFILE"
-        
-
+        write-host "rm $profileDir\profile"
+        write-host "cp .\windows_env\profile $profileDir"
     }
 }
 
